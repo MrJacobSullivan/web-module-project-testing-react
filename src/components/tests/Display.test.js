0cb8765128caad5c91fe7f639ce0e48c097162d4
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-08 18:46:33 -0600

    added tests for Display component

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,11 +1,72 @@
 import React from 'react'
+import { render, screen, act } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Display from './../Display'
 
-///Tasks:
-//1. Add in nessisary imports and values to establish the testing suite.
-//2. Test that the Display component renders without any passed in props.
-//3. Rebuild or copy a show test data element as used in the previous set of tests.
-//4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
-//5. Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.
-//6. Notice the optional functional prop passed in to the Display component client code. Test that when the fetch button is pressed, this function is called.
+import fetchShow from '../../api/fetchShow'
+jest.mock('../../api/fetchShow')
 
-test('sanity check', () => {})
+const testShow = {
+  name: 'test show name',
+  summary: 'test show summary',
+  seasons: [
+    {
+      id: 1,
+      name: 'season 1',
+      episodes: [],
+    },
+    {
+      id: 2,
+      name: 'season 2',
+      episodes: [],
+    },
+    {
+      id: 3,
+      name: 'season 3',
+      episodes: [],
+    },
+  ],
+}
+
+test('renders without any passed in props', () => {
+  render(<Display />)
+})
+
+test('when fetch button is pressed, Show component will render', async () => {
+  fetchShow.mockResolvedValueOnce(testShow)
+
+  render(<Display />)
+
+  const button = screen.getByRole('button')
+  userEvent.click(button)
+
+  const shows = await screen.findAllByTestId(/show-container/i)
+  expect(shows).toHaveLength(1)
+})
+
+test('when fetch button is pressed, correct number of select options are rendered', async () => {
+  fetchShow.mockResolvedValue(testShow)
+  const mockDisplayFunc = jest.fn()
+
+  render(<Display displayFunc={mockDisplayFunc} />)
+
+  const button = screen.getByRole('button')
+  userEvent.click(button)
+
+  const options = await screen.findAllByTestId(/season-option/i)
+  expect(options).toHaveLength(3)
+
+  // console.log(mockDisplayFunc)
+})
+
+test('when fetch button is pressed, function in props is invoked', async () => {
+  fetchShow.mockResolvedValue(testShow)
+  const mockDisplayFunc = jest.fn()
+
+  render(<Display displayFunc={mockDisplayFunc} />)
+
+  const button = screen.getByRole('button')
+  await act(async () => userEvent.click(button))
+
+  expect(mockDisplayFunc).toHaveBeenCalled()
+})
